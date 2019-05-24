a93fc5debfc6d2da31c59cb331a002b0b90a2235
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2019-05-24 18:52:48 +0100

    integrated third party library : ProgressHud for displaying beautiful alert messages upon answering a question

---
+++ Quizzler/Quizzler-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+/**STEPS TO IMPORT OTHER LIBRARIES TO OUR XCODE PROJECT*/
+
+//1. drag the cloned project to Supporting Files directory
+//2. creating the bridging header
+//3. importing the Project here here
+
+
+//we want to import the header file to this project file
+
+#import "ProgressHUD.h"
--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
     //helps to keep track which question the user is on
     var questionNumber : Int = 0
     
+    var score : Int = 0
+    
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -31,10 +34,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //remember, in swift the properties of an object are accessed using the dot notation
         //we want to display the first question upon loading the first screen of the app
-        let firstQuestion = allQuestions.list[0] //0 index grabs the data or first element of the array
+        //let firstQuestion = allQuestions.list[0] //0 index grabs the data or first element of the array
         
         //after grabbing the firstQuestion data from the model, display this question in the questionLabel on the screen
-        questionLabel.text = firstQuestion.questionText
+        //questionLabel.text = firstQuestion.questionText
+        
+        nextQuestion()
         
     }
     
@@ -68,7 +73,13 @@ class ViewController: UIViewController {
     
     //This method will update all the views on the screen (progress bar, score label, etc
     func updateUI() {
-      
+        scoreLabel.text = "Score : \(score)" //escape this value to string because score label expect a string instead of an integer
+        
+        progressLabel.text = "\(questionNumber + 1) /13"
+        
+        //get the width of the screen divided by 13
+        //the below calculation means the width of the default progress bar (yellow) bar should be equal to TOTAL WIDTH OF THE SCREEN DIVIDED BY 13 (count of all questions) multiplied by the current question the user is on
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
     //This method will update the question text field and check if we reached the end
@@ -77,22 +88,25 @@ class ViewController: UIViewController {
         //here we are just updating the question label to the next data questionText.. we are not changing to the next screen in anyways
         if questionNumber <= 12 {
             questionLabel.text = allQuestions.list[questionNumber].questionText
+            updateUI()
         } else {
             //displays allert message to the user
             let alert = UIAlertController(title: "Awesome!", message: "You've finished all the questions, do you want to start all over again ?", preferredStyle: .alert)
             
-            //this code gets executed when the user clicks i want to restart
+            //this code gets executed when the user clicks Restart pop up displayed to them
+//            basicaly this question returns the startOver function
             //basicaly UIAlertAction returns a closer (UIAlertAction) that returns or execute a block of code
             let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
                 
-                //note that when u r returning a closure u have to add the keyword self this helps
+                //note that when u r returning a closure u have to add the keyword self.. normally we are supposed to access the function directly without the self keyword but we need to use self keyword to access this function incase there is an existing function with thesame name in the UIAlertAction Class because we r in the closure
                 self.startOver()
             }
             
             //this executes the restartAction closure of UIAlertAction
             alert.addAction(restartAction)
             
-            print("End of Quiz")
+            //completion nill because we dont want anything to happen after presenting the alert to the user
+            present(alert, animated: true, completion: nil)
             //reset question number so the app can take the user back to question 0
             questionNumber = 0
         }
@@ -108,17 +122,24 @@ class ViewController: UIViewController {
         
         if correctAnswer == pickedAnswer {
             //print to the screen that the user got the answer
+            ProgressHUD.showSuccess("Correct!")
             print("You got it!")
+            score += 1
         } else {
+            ProgressHUD.showError("Wrong!")
             print("wrong!")
         }
     }
     
     
     func startOver() {
-       
+       //reset the question number that tracks which question we already on
+        //reset back to 0
+        questionNumber = 0
+        //call the next question method whenever we start all over
+        nextQuestion()
+        score = 0
+        scoreLabel.text = "Score : \(score)"
     }
-    
-
     
 }
